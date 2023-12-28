6cc2e5f396f539f6cfcbcbd996a7d98ca092e4ff
coderoofsit <coderoofitsolutions@gmail.com>
2023-12-28 09:12:37 +0530

    .

--- app/router/auth.py
+++ app/router/auth.py
@@ -29,3 +29,4 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
     access_token = oauth2.create_access_token(data={"user_id": user.id})
 
     return {"access_token": access_token, "token_type": "bearer"}
+# kkkk
